65d31ba849d1af06935cb2ae22fefe12f315fe3d
“Lucero <lhernandez392@insite.4cd.edu  git config --global user.name Lucero Hernandez  git config --global user.email lhernandez392@insite.4cd.edu>
2023-04-24 21:04:39 -0700

    updated for drowpdown features

--- j/main.js
+++ j/main.js
@@ -5,12 +5,72 @@
 // the format is: fetch().then().then().catch()
 //it's easier to read if we put each step in its own line,
 //that's why the periods start the then lines.
+// Character list. Each house has a name and a code
+const houses = [
+  {
+    code: "ST",
+    name: "Stark"
+  },
+  {
+    code: "LA",
+    name: "Lannister"
+  },
+  {
+    code: "BA",
+    name: "Baratheon"
+  },
+  {
+    code: "TA",
+    name: "Targaryen"
+  }
+];
 
+// Return an array of characters belonging to a house
+const getCharacters = houseCode => {
+  switch (houseCode) {
+    case "ST":
+      return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+    case "LA":
+      return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+    case "BA":
+      return ["Robert", "Stannis", "Renly"];
+    case "TA":
+      return ["Aerys", "Daenerys", "Viserys"];
+    default:
+      return []; // Empty array
+  }
+};
+  document.addEventListener('DOMContentLoaded', init);
+  
+    function init() {
+      let dropdown = document.getElementById('house');
+     
+    houses.forEach(house => {
+      let myoption = document.createElement('OPTION');
+      myoption.value = house.code;
+      let name = house.name;
+      myoption.innerHTML = name;
+      
+     dropdown.append(myoption);
+    });
+      
+      dropdown.addEventListener('change', (e) => {
+            const code = e.target.value;
+            const members = getCharacters(code);
+            let container = document.getElementById('characters');
+
+            members.forEach(person => {
+              let item = document.createElement('LI');
+              item.innerText = person;
+              container.append(item);
+            })
+      });
+  }; 
 fetch("houses.json")
     .then((response) => response.json())
-    .then((data) => {
+    .then((data) => { 
         //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "";
+        let html = document.getElementById(container);
 
         //the argument "house" passed to the arrow function
         //holds each item in the array in turn.
@@ -23,11 +83,12 @@ fetch("houses.json")
         <p class="folks">${family}</p>`;
             html += objInfo;
         });
-
+        document.getElementById("container").innerHTML = Object.values(houses);
         //make a reference to the html container where
         //the info will be displayed.
         const container = document.querySelector("#container");
-        container.innerHTML = html;
+        container.innerHTML = html; 
+    
     })
     .catch((err) => console.log("Oops!", err));
     //this only runs if there is an error during the above process
