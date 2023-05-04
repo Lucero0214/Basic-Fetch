a2dc5ad0c91f6774e8aac54b8fe56aff5b644195
“Lucero <lhernandez392@insite.4cd.edu  git config --global user.name Lucero Hernandez  git config --global user.email lhernandez392@insite.4cd.edu>
2023-05-03 20:35:09 -0700

    update code for console

--- j/main.js
+++ j/main.js
@@ -1,22 +1,15 @@
 
-//This code does NOT create any global variables.
-//Promises can be chained together, with the previous promise
-// passing its results to the next one in the chain.
-// the format is: fetch().then().then().catch()
-//it's easier to read if we put each step in its own line,
-//that's why the periods start the then lines.
-
 fetch("houses.json")
-    .then((response) => response.json())
+  .then((response) => response.json())
     .then((data) => {
         //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "";
+        let html = document.getElementById("container");
 
         //the argument "house" passed to the arrow function
         //holds each item in the array in turn.
         data.forEach((house) => {
             let family = house.members.join(" | ");
-
+            console.log(family)
             // generate the html snippet for one array item
             //to be added to the "html" temp holder.
             let objInfo = `<p class="house">${house.name}</p>
@@ -24,10 +17,14 @@ fetch("houses.json")
             html += objInfo;
         });
 
+        document.getElementById("container").innerHTML = Object.values(houses);
         //make a reference to the html container where
         //the info will be displayed.
         const container = document.querySelector("#container");
-        container.innerHTML = html;
-    })
-    .catch((err) => console.log("Oops!", err));
+        container.innerHTML = html;  
+      })
+      .catch((data) => console.log("data"))
+  {
+    console.log("error")
+  }
     //this only runs if there is an error during the above process
